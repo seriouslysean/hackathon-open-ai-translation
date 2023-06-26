22c404d480ccdc01c19ab364697c3c8dfb97ed77
Sean Kennedy <skennedy@urbn.com>
2023-06-26 14:58:53 -0400

    Convert get to post, add languages

--- app.vue
+++ app.vue
@@ -1,22 +1,81 @@
 <script setup>
-const { data } = await useFetch('/api/translate');
+const languages = ref([
+  { name: 'Choose Locale', locale: '' },
+  { name: 'English (en-GB)', locale: 'en-GB' },
+  { name: 'French (fr-CA)', locale: 'fr-CA' },
+  { name: 'German (de-DA)', locale: 'de-DE' },
+  { name: 'Italian (it-IT)', locale: 'it-IT' },
+  { name: 'Spanish (es-ES)', locale: 'es-ES' },
+]);
+
+const textEl = ref(null);
+const text = ref('');
+const locale = ref('');
+const translation = ref('');
+
+async function onSubmit() {
+  if (!text.value || !locale.value) {
+    alert('Text and locale required');
+    return;
+  }
+  // TODO Convert to composable maybe `useTranslation(text, locale)`
+  const { data: translatedText } = await useFetch('/api/translate', {
+    method: 'post',
+    query: {
+      text,
+      locale,
+    },
+  });
+  translation.value = translatedText.value;
+}
+
+function onCopy() {
+  if (location.protocol !== 'https:') {
+    alert('Copy will only work over https');
+    return;
+  }
+  navigator.clipboard.writeText(translation.value);
+}
+
+onMounted(() => {
+  textEl.value.focus();
+});
 </script>
 
 <template>
   <div class="app">
-    <h1>OpenAI Translation</h1>
-    <p><strong>Input (EN):</strong> Fly free with products that are light on you, and light on your wallet</p>
-    <p><strong>Output (FR):</strong> {{ data.text }}</p>
+    <h1>URBN Hackathon SEO Translations</h1>
 
-    <!-- <form @submit="onSubmit">
-      <label for="from">English</label>
-      <input type="text" name="from" />
+    <form @submit.prevent="onSubmit">
+      <div class="row">
+        <label for="text">Text (in English)</label>
+        <input ref="textEl" type="text" name="text" v-model="text" />
+      </div>
 
-      <label for="from">English</label>
-      <input type="text" name="from" />
+      <div class="row">
+        <label for="locale">Translate To</label>
+        <select name="locale" v-model="locale">
+          <option v-for="language in languages" :key="language.locale" :value="language.locale" :selected="locale">
+            {{ language.name }}
+          </option>
+        </select>
+      </div>
 
-      <label for="from">English</label>
-      <input type="text" name="from" />
-    </form> -->
+      <div class="row">
+        <button type="submit">Submit</button>
+        <button type="reset">Reset</button>
+      </div>
+    </form>
+
+    <hr>
+
+    <textarea readonly>{{ translation }}</textarea>
+    <button @click="onCopy">Copy</button>
   </div>
 </template>
+
+<style>
+label {
+  display: block;
+}
+</style>
--- server/api/translate.post.ts
+++ server/api/translate.post.ts
@@ -1,6 +1,6 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-async function translate() {
+async function translate(text: string, locale: string): Promise<String> {
     const apiKey = process.env.OPENAI_API_KEY;
     try {
       if (!apiKey) {
@@ -10,27 +10,30 @@ async function translate() {
       const openai = new OpenAIApi(configuration);
       const response = await openai.createCompletion({
           model: 'text-davinci-003',
-          prompt: 'Translate this from English in to French "Fly free with products that are light on you, and light on your wallet"',
+          prompt: `Translate the text delimited by triple pipes from en-US to ${locale}.
+
+|||${text}|||`,
           temperature: 0.3,
           max_tokens: 100,
           top_p: 1.0,
           frequency_penalty: 0.0,
           presence_penalty: 0.0,
       });
-      return {
-        text: response.data?.choices?.[0]?.text || 'unknown',
-      };
+      // console.log('Open AI response', JSON.stringify(response.data, null, 2));
+      return response.data?.choices?.[0]?.text?.trim() || 'unknown';
     } catch {
-      return {
-        text: 'unknown',
-      };
+      return 'unknown';
     }
 }
 
 
 export default defineEventHandler(async (event) => {
-    console.log('New request: ' + getRequestURL(event))
-    const translation = await translate();
-    console.log('!!! translation', translation);
+    const query = getQuery(event);
+    const text: string = query.text as string;
+    const locale: string = query.locale as string;
+    if (!text || !locale) {
+      throw new Error('Text and locale required');
+    }
+    const translation = await translate(text, locale);
     return translation;
 });
